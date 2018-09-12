492eb71eb8ca65741526b383a6705de9ff1ec092
Mihail Fernandes <fernandesbg88@gmail.com>
2018-09-12 12:38:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const path       = require('path')
 const express    = require('express')
 const bodyParser = require('body-parser')
 const Chatkit    = require('pusher-chatkit-server')
+
+const app = express()
+const chatkit = new Chatkit.default(require({
+    instanceLocator: "PUSHER_CHATKIT_INSTANCE_LOCATOR",
+    key: "PUSHER_CHATKIT_KEY"
+}))
