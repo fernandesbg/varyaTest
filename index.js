8d4d668b3a6b6492cbcb9d8d0f69baf284263eca
Mihail Fernandes <fernandesbg88@gmail.com>
2018-09-12 13:02:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const Chatkit    = require('pusher-chatkit-server')
 
 const app = express()
 const chatkit = new Chatkit.default(require({
-    instanceLocator: "PUSHER_CHATKIT_INSTANCE_LOCATOR",
-    key: "PUSHER_CHATKIT_KEY"
+    instanceLocator: "v1:us1:f1e97e70-2ad9-4984-879d-56bd5f90699c",
+    key: "42179b09-1889-4cea-ba60-c977b3aba533:hjMKjY8vNdgc0i8POkwbHRTCeritVFjEAiVeLu1bLaQ="
 }))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'assets')))
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {root: __dirname + '/views'})
+})
